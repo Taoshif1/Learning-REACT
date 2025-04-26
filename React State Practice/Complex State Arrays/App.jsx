8cbff41cf84d0f468a8e4f32127c0b5f8c04adbf
Taoshif1 <161155604+Taoshif1@users.noreply.github.com>
2025-04-26 20:04:38 +0600

    Update App.jsx

--- React State Practice/Complex State Arrays/App.jsx
+++ React State Practice/Complex State Arrays/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
         setMyFavoriteThings(
             prevFavThings => [
                 ...prevFavThings,
-                "Test"
+                allFavoriteThings[prevFavThings.length]
             ]
         )
     }
